645abb4652e2a96bf2ab660fc9bb376873cdd4b7
jonathan-henrique <jonathan.henrique@senior.com.br>
2019-09-26 12:51:53 -0300

    Using spring profiles per environment's development and production

--- src/main/java/com/algaworks/algafood/di/notification/EmailNotifier.java
+++ src/main/java/com/algaworks/algafood/di/notification/EmailNotifier.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.di.notification;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.model.Customer;
@@ -7,9 +8,14 @@ import com.algaworks.algafood.di.model.Customer;
 @NotifierType(PriorityType.MINIMUN)
 @Component
 public class EmailNotifier implements Notifier {
+		
+	@Autowired
+	private NotifierProperties properties;
 	
 	@Override
 	public void notify(Customer customer, String message) {
+		System.out.println("Host:" + properties.getHostServer());
+		System.out.println("Port:" + properties.getPortServer());
 		System.out.printf("Notificando %s através do e-mail %s: %s\n",
 				customer.getName(), customer.getEmail(), message);
 	}
