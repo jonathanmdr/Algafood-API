235ab159f4aa08ebac0fd328e3b457316a6a2e9a
jonathan-henrique <jonathan.henrique@senior.com.br>
2019-09-30 10:40:54 -0300

    Add spring data JPA and connection JDBC MySQL

--- src/main/java/com/algaworks/algafood/di/notification/Notifier.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.algafood.di.notification;
-
-import com.algaworks.algafood.di.model.Customer;
-
-public interface Notifier {
-
-	void notify(Customer customer, String message);
-
-}
--- src/main/java/com/algaworks/algafood/di/notification/PriorityType.java
+++
@@ -1,6 +0,0 @@
-package com.algaworks.algafood.di.notification;
-
-public enum PriorityType {
-	MINIMUN, 
-	MAXIMUN
-}
