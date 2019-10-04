581c203d2308a7e93e278a1cf762edae91bf9812
jonathan-henrique <jonathan.henrique@senior.com.br>
2019-10-04 12:50:16 -0300

    Added method findById on StateController

--- src/main/java/com/algaworks/algafood/api/controller/StateController.java
+++ src/main/java/com/algaworks/algafood/api/controller/StateController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class StateController {
 	@GetMapping
 	public List<State> findAll() {
 		return stateRepository.list();
+	}
+	
+	@GetMapping("/{stateId}")
+	public State findById(@PathVariable Long stateId) {
+		return stateRepository.findById(stateId);
 	}
 
 }
