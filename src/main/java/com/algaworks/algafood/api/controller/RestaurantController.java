111bf9b1457f935d834ab48e0a4929f1e277d5be
jonathan-henrique <jonathan.henrique@senior.com.br>
2019-10-17 10:08:55 -0300

    Added new resource for update objetcs partial usinp PATCH mapping and API reflections

--- src/main/java/com/algaworks/algafood/api/controller/RestaurantController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestaurantController.java
@@ -1,13 +1,17 @@
 package com.algaworks.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,6 +24,7 @@ import com.algaworks.algafood.domain.exception.EntityNotFoundException;
 import com.algaworks.algafood.domain.model.Restaurant;
 import com.algaworks.algafood.domain.repository.RestaurantRepository;
 import com.algaworks.algafood.domain.service.RestaurantService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/restaurants")
@@ -54,12 +59,12 @@ public class RestaurantController {
 	
 	@PutMapping("/{restaurantId}")
 	public ResponseEntity<Restaurant> update(@PathVariable Long restaurantId, @RequestBody Restaurant restaurant) {
-		Restaurant RestaurantSaved = restaurantRepository.findById(restaurantId);
+		Restaurant restaurantSaved = restaurantRepository.findById(restaurantId);
 		
-		if (RestaurantSaved != null) {
-			BeanUtils.copyProperties(restaurant, RestaurantSaved, "id");
-			RestaurantSaved =  restaurantService.save(RestaurantSaved);
-			return ResponseEntity.ok(RestaurantSaved);
+		if (restaurantSaved != null) {
+			BeanUtils.copyProperties(restaurant, restaurantSaved, "id");
+			restaurantSaved =  restaurantService.save(restaurantSaved);
+			return ResponseEntity.ok(restaurantSaved);
 		}
 		
 		return ResponseEntity.notFound().build();
@@ -75,6 +80,33 @@ public class RestaurantController {
 		} catch(EntityInUseException ex) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
+	}
+	
+	@PatchMapping("/retaurantId")
+	public ResponseEntity<?> updatePartial(@PathVariable Long restaurantId, @RequestBody Map<String, Object> values) {
+		Restaurant restaurantSaved = restaurantRepository.findById(restaurantId);
+		
+		if (restaurantSaved == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		merge(values, restaurantSaved);
+		
+		return update(restaurantId, restaurantSaved);
+	}
+	
+	private void merge(Map<String, Object> valuesOrigin, Restaurant restaurantDestiny) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Restaurant restaurantOrigin = objectMapper.convertValue(valuesOrigin, Restaurant.class);
+		
+		valuesOrigin.forEach((key, value) -> {
+			Field field = ReflectionUtils.findField(Restaurant.class, key);
+			field.setAccessible(true);
+			
+			Object newValue = ReflectionUtils.getField(field, restaurantOrigin);
+			
+			ReflectionUtils.setField(field, restaurantDestiny, newValue);
+		});
 	}
 
 }
