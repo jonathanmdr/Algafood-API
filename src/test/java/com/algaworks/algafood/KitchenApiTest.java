a6985b0f1b7c547e8aa39d68340f76be1331b520
Jonathan Henrique <jonathan.mdr@hotmail.com>
2019-11-21 19:32:27 -0300

    Adjustment in the order of execution of tests reestablishing the database for each test

--- src/test/java/com/algaworks/algafood/KitchenApiTest.java
+++ src/test/java/com/algaworks/algafood/KitchenApiTest.java
@@ -2,10 +2,12 @@ package com.algaworks.algafood;
 
 import static org.hamcrest.CoreMatchers.hasItems;
 
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -21,11 +23,16 @@ public class KitchenApiTest {
 	@LocalServerPort
 	private int port;
 	
+	@Autowired
+	private Flyway flyway;
+	
 	@Before
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.basePath = "/kitchens";
 		RestAssured.port = this.port;
+		
+		flyway.migrate();
 	}
 	
 	@Test
@@ -48,6 +55,18 @@ public class KitchenApiTest {
 			.statusCode(HttpStatus.OK.value())
 			.body("name", Matchers.hasSize(4))
 			.body("name", hasItems("Indiana", "Tailandesa"));
+	}
+	
+	@Test
+	public void returningStatus201WhenSaveKitchen() {		
+		RestAssured.given()
+			.body("{ \"name\": \"Chinesa\" }")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
 	}
 
 }
