8d051d31b9ee4b12a2e2cf80b15885cc341affd9
jonathan-henrique <jonathan.henrique@senior.com.br>
2020-02-04 07:38:15 -0300

    Bugfix in documentations

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -72,10 +72,11 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 					  new Tag("Grupos", "Realiza o gerenciamento de grupos de usuário"),
 					  new Tag("Cozinhas", "Realiza o gerencimento de cozinhas"),
 					  new Tag("Formas de pagamento", "Realiza o gerencimento de formas de pagamento"),
-					  new Tag("Pedidos", "Realiza o gerencimento de pedidos"),
+					  new Tag("Restaurantes", "Realiza o gerencimento de restaurantes"),
+					  new Tag("Pedidos", "Realiza o gerencimento de pedidos"),					  
 					  new Tag("Estados", "Realiza o gerencimento de estados"),
 					  new Tag("Produtos", "Realiza o gerenciamento de produtos"),
-					  new Tag("Usuários", "Realiza o gerencimento de usuários"),
+					  new Tag("Usuários", "Realiza o gerenciamento de usuários"),
 					  new Tag("Estatísticas", "Consulta estatísticas de vendas do Algafood"));
 	}
 	
