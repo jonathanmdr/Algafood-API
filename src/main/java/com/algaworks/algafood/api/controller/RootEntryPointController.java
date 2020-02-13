0640923d2583c73dad7facd775a1c0ff677912fd
jonathan-henrique <jonathan.henrique@senior.com.br>
2020-02-13 13:32:45 -0300

    Added link's hypermidea in root entry point resource

---
+++ src/main/java/com/algaworks/algafood/api/controller/RootEntryPointController.java
@@ -0,0 +1,40 @@
+package com.algaworks.algafood.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.api.AlgaLinks;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class RootEntryPointController {
+	
+	@Autowired
+	private AlgaLinks algaLinks;
+	
+	@GetMapping
+	public RootEntryPointDTO root() {
+		var rootEntyPointDto = new RootEntryPointDTO();
+		
+		rootEntyPointDto.add(algaLinks.linkToKitchens("kitchens"));
+		rootEntyPointDto.add(algaLinks.linkToOrders("orders"));
+		rootEntyPointDto.add(algaLinks.linkToRestaurants("restaurants"));
+		rootEntyPointDto.add(algaLinks.linkToGroups("groups"));
+		rootEntyPointDto.add(algaLinks.linkToUsers("users"));
+		rootEntyPointDto.add(algaLinks.linkToPermissions("permissions"));
+		rootEntyPointDto.add(algaLinks.linkToPaymentForms("payment-forms"));
+		rootEntyPointDto.add(algaLinks.linkToCities("cities"));
+		rootEntyPointDto.add(algaLinks.linkToStates("states"));
+		
+		return rootEntyPointDto;
+	}
+	
+	private static class RootEntryPointDTO extends RepresentationModel<RootEntryPointDTO> {
+		
+	}
+
+}
