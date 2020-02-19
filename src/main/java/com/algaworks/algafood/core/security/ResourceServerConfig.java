e702e134da800225a44b3a3f84a296751b340076
jonathan-henrique <jonathan.henrique@senior.com.br>
2020-02-19 11:07:27 -0300

    Validating public key in JWT for all requests

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,14 +1,9 @@
 package com.algaworks.algafood.core.security;
 
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -22,12 +17,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			.and() //
 			.cors().and() //
 			.oauth2ResourceServer().jwt();
-	}
-	
-	@Bean
-	public JwtDecoder jwtDecoder() {
-		var secretKet = new SecretKeySpec("S1DF981s1dg9s51c69S58DF1G1fdgdf6fgs9d1SDFG5".getBytes(), "HmacSHA256");
-		return NimbusJwtDecoder.withSecretKey(secretKet).build();
 	}
 
 }
