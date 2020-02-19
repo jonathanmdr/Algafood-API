5841cf1b097c2794f2b77624ba8951d94b6bb5c8
jonathan-henrique <jonathan.henrique@senior.com.br>
2020-02-19 09:11:42 -0300

    Using JWT with simetric key (HMAC SHA-256)

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package com.algaworks.algafood.core.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +20,14 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests() //				
 				.anyRequest().authenticated() //
 			.and() //
-			.oauth2ResourceServer().opaqueToken();
+			.cors().and() //
+			.oauth2ResourceServer().jwt();
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		var secretKet = new SecretKeySpec("S1DF981s1dg9s51c69S58DF1G1fdgdf6fgs9d1SDFG5".getBytes(), "HmacSHA256");
+		return NimbusJwtDecoder.withSecretKey(secretKet).build();
 	}
 
 }
